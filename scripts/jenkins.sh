a8f118c6cd9df1c6411a8660022e721ff0fd779e
Chris Lima <christopher@inatel.br>
2022-10-18 00:55:01 -0300

    Update jenkins.sh

--- scripts/jenkins.sh
+++ scripts/jenkins.sh
@@ -1,3 +1,1 @@
-echo "Mandando e-mail com o mail do linux" | mail -s "subject: qualquer coisa"
-
-
+echo "Mandando e-mail com o mail do linux" | mail -s "subject: qualquer coisa" chris.souzalima@outlook.com
