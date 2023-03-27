1563eccb4ded6b568e126602bd008cfbd70d3642
Chrislima <christopher@inatel.br>
2023-03-27 17:37:28 -0300

    Removing old files

--- scripts/jenkins.sh
+++
@@ -1,1 +0,0 @@
-echo "Mandando e-mail com o mail do linux" | mail -s "subject: qualquer coisa" chris.souzalima@outlook.com
--- scripts/shell.sh
+++
@@ -1,7 +0,0 @@
-ls
-cd Aula-GitHub-Actions
-echo "Inicio da instalação"
-sudo apt-get install mailutils
-echo "Fim da instalação"
-echo "Mandando e-mail com o mail do linux" | mail -s "subject: qualquer coisa" chris.souzalima@outlook.com
-
