d78de54c58a9351441b434c5a924259b85d0db3e
Chris Lima <christopher@inatel.br>
2023-03-27 20:31:57 -0300

    Adicionando scripts

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -2,4 +2,6 @@ ls
 cd Aula-GitHub-Actions
 ls
 echo "HAHAHAHAHAH"
+sudo apt install mailutils
+echo "fim da instalacao"
 echo "Mandando e-mail com mail do linux" | mail -s "meu email deu tudo certo" christopher@inatel.br
