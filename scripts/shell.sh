5ec05341281c36d17701192ced799d83afa0d97d
Chris Lima <christopher@inatel.br>
2023-03-27 20:35:06 -0300

    Adicionando scripts

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -4,4 +4,4 @@ ls
 echo "HAHAHAHAHAH"
 sudo apt install mailutils
 echo "fim da instalacao"
-echo "Mandando e-mail com mail do linux" | mail -s "meu email deu tudo certo" christopher@inatel.br
+echo "Mandando e-mail com mail do linux" | mail -s "meu email deu tudo certo" chris.souzalima@outlook.com
