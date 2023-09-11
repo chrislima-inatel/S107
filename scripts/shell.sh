cb1f311aaade87a381efa39c97f4d18ec0bb9045
Chris Lima <christopher@inatel.br>
2023-09-11 20:43:21 -0300

    Add novo script - agora vai2

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -1,4 +1,8 @@
 ls
 cd Aula-GitHub-Actions
 ls
-pwd
+echo "AAAHAHAHAAA"
+sudo apt-get install mailutils
+echo "Fim da instalacao"
+echo "Mandando e-mail com mail do linux" | mail -s "a subject" ${EMAIL_LIST}
+# posso fazer o que eu quiser aqui
