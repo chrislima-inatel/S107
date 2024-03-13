1980934cee4835d436b46e7ce4a354a4b4e60346
Chris Lima <christopher@inatel.br>
2024-03-13 20:37:56 -0300

    Notificacao

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -4,5 +4,5 @@ ls
 echo "AAAHAHAHAAA"
 sudo apt-get install mailutils
 echo "Fim da instalacao"
-echo "Mandando e-mail com mail do linux" | mail -s "a subject" ${EMAIL_LIST}
+echo "Mandando e-mail com mail do linux" | mail -s "a subject" christopher@inatel.br
 # posso fazer o que eu quiser aqui
